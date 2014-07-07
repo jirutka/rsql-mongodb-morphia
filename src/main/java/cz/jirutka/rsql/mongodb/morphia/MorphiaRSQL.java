48f6628c7b8622298d6f2d84772657cb71a3ba0a
Jakub Jirutka <jakub@jirutka.cz>
2014-07-07 20:52:51 +0200

    Extract interface from MorphiaRSQL class

--- src/main/java/cz/jirutka/rsql/mongodb/morphia/MorphiaRSQL.java
+++ src/main/java/cz/jirutka/rsql/mongodb/morphia/MorphiaRSQL.java
@@ -23,76 +23,12 @@
  */
 package cz.jirutka.rsql.mongodb.morphia;
 
-import cz.jirutka.rsql.mongodb.parser.MongoRSQLNodesFactory;
-import cz.jirutka.rsql.parser.RSQLParser;
-import cz.jirutka.rsql.parser.RSQLParserException;
-import cz.jirutka.rsql.parser.ast.Node;
-import lombok.Getter;
-import lombok.Setter;
-import org.mongodb.morphia.Datastore;
-import org.mongodb.morphia.DatastoreImpl;
-import org.mongodb.morphia.mapping.Mapper;
 import org.mongodb.morphia.query.Criteria;
 import org.mongodb.morphia.query.Query;
 
-public class MorphiaRSQL {
+public interface MorphiaRSQL {
 
-    @Getter
-    private final Datastore datastore;
+    Criteria createCriteria(String rsql, Class<?> entityClass);
 
-    @Getter @Setter
-    private StringConverter converter = new DefaultStringConverter();
-
-    @Getter @Setter
-    private RSQLParser rsqlParser = new RSQLParser(new MongoRSQLNodesFactory());
-
-    // lazy initialized
-    private Mapper mapper;
-
-
-    public MorphiaRSQL(Datastore datastore) {
-        this.datastore = datastore;
-    }
-
-
-    public Criteria createCriteria(String rsql, Class<?> entityClass) {
-
-        Node rootNode = parse(rsql);
-
-        MorphiaRSQLVisitor visitor = new MorphiaRSQLVisitor(entityClass, getMapper(), converter);
-
-        return rootNode.accept(visitor);
-    }
-
-    public <T> Query<T> createQuery(String rsql, Class<T> entityClass) {
-
-        Node rootNode = parse(rsql);
-
-        Query<T> query = datastore.createQuery(entityClass);
-        MorphiaRSQLVisitor visitor = new MorphiaRSQLVisitor(entityClass, getMapper(), converter);
-
-        query.and(rootNode.accept(visitor));
-
-        return query;
-    }
-
-
-    protected Node parse(String rsql) {
-        try {
-            return rsqlParser.parse(rsql);
-
-        } catch (RSQLParserException ex) {
-            throw new RSQLException(ex);
-        }
-    }
-
-    private Mapper getMapper() {
-        if (mapper == null) {
-            if (! (datastore instanceof DatastoreImpl)) {
-                throw new IllegalStateException("datestore is not instance of DatastoreImpl");
-            }
-            mapper = ((DatastoreImpl) datastore).getMapper();
-        }
-        return mapper;
-    }
+    <T> Query<T> createQuery(String rsql, Class<T> entityClass);
 }
